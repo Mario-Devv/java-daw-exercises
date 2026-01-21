4a67d40a1122b3ec9a083dba50fa7f50b6d8d791
Mario-Devv <marioodevv@gmail.com>
2026-01-21 10:35:42 +0100

    Finish proyect 4

--- test4/RA4 exercises/src/Main.java
+++ test4/RA4 exercises/src/Main.java
@@ -1,5 +1,25 @@
+import Numero.Numero;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hola mundo");
+        // Abrimos scanner
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce un número");
+
+        // Almacenamos valor del scanner
+        int valorUser = sc.nextInt();
+
+        // Llamos a nuestro objeto y le asignamos el valor del scanner
+        Numero miNumero = new Numero(valorUser);
+
+
+        System.out.println("EL doble es: "+ miNumero.getDoble());
+        System.out.println("El triple es: "+ miNumero.getTriple());
+
+        // Le añadimos 15 más como pide
+        miNumero.suma(15);
+        System.out.println("Y le sumo 15, son: "+ miNumero.getNumero());
     }
 }
--- test4/RA4 exercises/src/Numero/Numero.java
+++ test4/RA4 exercises/src/Numero/Numero.java
@@ -1,4 +1,51 @@
 package Numero;
 
 public class Numero {
+    //Creamos el atributo
+    private int numero;
+
+    //Creamos constructor por defecto con parametros
+    public Numero(int numero) {
+        this.numero = numero;
+    }
+
+    // metodos
+
+    // Obtener numero
+    public int getNumero(){
+        return numero;
+    }
+
+    // Añadir valor de numero al atributo
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public void suma(int mas) {
+        int miSuma = numero + mas;
+
+        // Actualizamos valor de numero
+        numero = miSuma;
+    }
+
+    public void resta(int resta) {
+        int miResta = numero - resta;
+
+        // Actualizamos valor de numero
+        numero = miResta;
+    }
+
+    // Doble del numero
+    public int getDoble (){
+        return getNumero() * 2;
+    }
+
+    // Obtener el triple
+    public int getTriple () {
+        return getNumero() * 3;
+    }
+
+
+
+
 }
