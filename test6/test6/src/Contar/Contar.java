7135df5f29b1d18b65abbc730d119c8bc7128214
Mario-Devv <marioodevv@gmail.com>
2026-01-24 17:59:33 +0100

    exercise 6

---
+++ test6/test6/src/Contar/Contar.java
@@ -0,0 +1,35 @@
+package Contar;
+
+public class Contar {
+
+    // Atributo
+    private int contador;
+
+    // Constructor por defecto
+    public Contar () {
+        contador = 0;
+    }
+
+    // Constructor con parámetros
+    public Contar (int contador) {
+        this.contador = contardor;
+    }
+
+    //Setters
+    public void setContador (int contador) {
+        this.contador = contador;
+    }
+
+    //Gets
+    public int getContador () {
+        return contador;
+    }
+
+
+    //Métodos
+    public void incrementar (int incremento) {
+        incremento++;
+        incremento = contador;
+    }
+
+}
