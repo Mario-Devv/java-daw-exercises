b10c2d5cce37a8c7ef16dc03b6a37c4e2131f8b9
Mario-Devv <marioodevv@gmail.com>
2026-02-16 13:53:48 +0100

    regex exer

--- ejercicio-03/ejercicio-03/src/Main.java
+++ ejercicio-03/ejercicio-03/src/Main.java
@@ -7,9 +7,10 @@ class ValidadorDatos {
         String email = "mario@gmail.com";
         String telefono = "666123456";
 
-        String regexEmail = "\\d{8}[a-zA-Z]{1}";
+        //Regex para el dni
+        String regexDNI = "\\d{8}[a-zA-Z]{1}"; //regex formato dni ESP
 
-        Pattern patter = Pattern.compile(regexEmail);
+        Pattern patter = Pattern.compile(regexDNI);
         Matcher matcher = patter.matcher(dni);
 
         if(matcher.matches()) {
@@ -17,6 +18,32 @@ class ValidadorDatos {
             System.out.println(dni);
         } else {
             System.out.println("Parece que hay un pequeño error con el dni, revisalo");
+        }
+        //Fin dni
+
+        // Regex para email
+        String regexEmail = "[a-zA-Z0-9]+@[a-zA-Z]+\\.[a-zA-Z]+";
+
+        Pattern pEmail = Pattern.compile(regexEmail);
+        Matcher matcherEmail = pEmail.matcher(email);
+
+        if(matcherEmail.matches()) {
+            System.out.println("Formato del correo correcto: "+ email);
+        } else {
+            System.out.println("Formato del correo incorrecto, por favor intentelo de nuevo");
+        }
+
+        // FIN regex email
+
+        // Regex tlf
+        String regexTelefono = "[6-9]{1}\\d{8}";
+        Pattern pTelefono = Pattern.compile(regexTelefono);
+        Matcher matcherTelefono = pTelefono.matcher(telefono);
+
+        if(matcherTelefono.matches()) {
+            System.out.println("Número de teléfono aceptado "+ telefono);
+        } else {
+            System.out.println("Vaya parece que el número de teléfono no es correcto, por favor compruebelo");
         }
     }
 }
