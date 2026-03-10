a53668e71861e23ce065ab39f5d67e9cf6b49e56
Mario-Devv <marioodevv@gmail.com>
2026-03-10 10:03:41 +0100

    test03

---
+++ RA07/RA07/src/test03/Main.java
@@ -0,0 +1,12 @@
+package test03;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Mano mano = new Mano();
+        // Mostrar los dedos de la mano
+        System.out.println("Los dedos de la mano son:");
+        mano.mostrarDedos();
+    }
+}
